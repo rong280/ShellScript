08f186c65ef3502b307bc88b41dea0f2f3bafdcb
vincent-H <46416530+rong280@users.noreply.github.com>
2021-05-22 20:19:51 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+GREEN="\033[32m"
+COLOREND="\033[0m"
+
+startmenu(){
+clear
+echo -e "
+                                               ████
+  ${GREEN}Author: ${COLOREND}Vincent Huang        |              █░░░ █                   ██   ██
+  ${GREEN}Date:   ${COLOREND}2021-03-27           |     ██    ██░    ░█ ██████  ██████   ░░██ ██
+  ${GREEN}Blog:   ${COLOREND}www.linux321.com     |    ░██   ░██   ███ ░░██░░█ ░░░░░░██   ░░███
+  ${GREEN}Mail:   ${COLOREND}command@111.com      |    ░░██ ░██   █░░   ░██ ░   ███████    ░██
+  ${GREEN}Github: ${COLOREND}github.com/rong280   |     ░░████   █      ░██    ██░░░░██    ██
+  ${GREEN}V2ray:  ${COLOREND}www.v2ray.com        |      ░░██   ░██████░███   ░░████████  ██
+                                        ░░    ░░░░░░ ░░░     ░░░░░░░░  ░░      "
+}
+startmenu
