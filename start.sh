c541a165eee0d1461987a5d29cc9f4be0307a354
vincent-H <46416530+rong280@users.noreply.github.com>
2021-05-22 21:04:39 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 GREEN="\033[32m"
+BIUE="\033[36;5m"
 COLOREND="\033[0m"
 
 v2ray(){
 clear
 echo -e "
-                                               ████
-  ${GREEN}Author: ${COLOREND}Vincent Huang        |              █░░░ █                   ██   ██
-  ${GREEN}Date:   ${COLOREND}2021-05-22           |     ██    ██░    ░█ ██████  ██████   ░░██ ██
-  ${GREEN}Blog:   ${COLOREND}www.linux321.com     |    ░██   ░██   ███ ░░██░░█ ░░░░░░██   ░░███
-  ${GREEN}Mail:   ${COLOREND}command@111.com      |    ░░██ ░██   █░░   ░██ ░   ███████    ░██
-  ${GREEN}Github: ${COLOREND}github.com/rong280   |     ░░████   █      ░██    ██░░░░██    ██
-  ${GREEN}V2ray:  ${COLOREND}www.v2ray.com        |      ░░██   ░██████░███   ░░████████  ██
-                                        ░░    ░░░░░░ ░░░     ░░░░░░░░  ░░      "
+                                               ${BIUE}████${COLOREND}
+  ${GREEN}Author: ${COLOREND}Vincent Huang        |              ${BIUE}█░░░ █                   ██   ██${COLOREND}
+  ${GREEN}Date:   ${COLOREND}2021-03-27           |     ${BIUE}██    ██░    ░█ ██████  ██████   ░░██ ██${COLOREND}
+  ${GREEN}Blog:   ${COLOREND}www.linux321.com     |    ${BIUE}░██   ░██   ███ ░░██░░█ ░░░░░░██   ░░███${COLOREND}
+  ${GREEN}Mail:   ${COLOREND}command@111.com      |    ${BIUE}░░██ ░██   █░░   ░██ ░   ███████    ░██${COLOREND}
+  ${GREEN}Github: ${COLOREND}github.com/rong280   |     ${BIUE}░░████   █      ░██    ██░░░░██    ██${COLOREND}
+  ${GREEN}V2ray:  ${COLOREND}www.v2ray.com        |      ${BIUE}░░██   ░██████░███   ░░████████  ██${COLOREND}
+                                        ${BIUE}░░    ░░░░░░ ░░░     ░░░░░░░░  ░░${COLOREND}"
 }
 
 if [ $1 = v2ray ];then
